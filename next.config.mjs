18d813d3f57979dbf025107d0e52b5dcb77c272f
Jaden Balogh <jadenbalogh@gmail.com>
2024-08-07 21:59:36 -0400

    Revert "Add conditional next configurations for debug and release"
    This reverts commit ef6013a3e808320e327a43907ae38e26b3480158.

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
-
-const config = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: false,
-    };
-  }
-
-  return {
-    basePath: '/badminton-court-scheduler',
-    output: 'export',
-    reactStrictMode: false,
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/badminton-court-scheduler",
+  output: "export",
+  reactStrictMode: false,
 };
 
-export default config;
+export default nextConfig;
