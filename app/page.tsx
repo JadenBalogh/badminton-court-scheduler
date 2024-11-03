801747d1e9f634c3b966f8492751b13033281899
Jaden Balogh <jadenbalogh@gmail.com>
2024-11-03 08:34:03 -0500

    Upload Nov 3 player data

--- app/page.tsx
+++ app/page.tsx
@@ -149,7 +149,7 @@ export default function Home() {
       return; // Players are already loaded, don't overwrite
     }
 
-    let data = await fetch('./registered-players-oct27.txt');
+    let data = await fetch('./registered-players-nov3.txt');
     let text = await data.text();
 
     activePlayers = [];
