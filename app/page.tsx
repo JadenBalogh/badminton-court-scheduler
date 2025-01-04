615ab0c198d33bf548c188302df37e1df888b513
Jaden Balogh <jadenbalogh@gmail.com>
2025-01-04 15:42:46 -0500

    Upload Dec5 players

--- app/page.tsx
+++ app/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
       return; // Players are already loaded, don't overwrite
     }
 
-    let data = await fetch('./registered-players-nov24.txt');
+    let data = await fetch('./registered-players-dec5.txt');
     let text = await data.text();
 
     activePlayers = [];
